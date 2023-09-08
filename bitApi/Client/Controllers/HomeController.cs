7beb1473639a4a41f917ba1cad4652acd609e61d
CARLOS JHOAN TORIBIO CAMACHO <sd-17-30452@ufhec.edu.do>
2023-09-08 17:31:48 -0400

    Se realizo el proyecto aparte en la solucion para obtener y filtrar los resultados de la bd, con los datos obtenidos de la api.

---
+++ bitApi/Client/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Client.Data;
+using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        ClientContext _context;
+        public HomeController(ILogger<HomeController> logger, ClientContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index(string s)
+        {
+            if(s == null)
+            {
+                return View(_context.announcement.OrderByDescending(a=>a.date));
+            }
+            else
+            {
+                return View(_context.announcement.Where(a => a.title.Contains(s) || a.content.Contains(s)).OrderByDescending(a => a.date));
+            }
+            
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ bitApi/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Client.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ClientContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("bitApiContext") ?? throw new InvalidOperationException("Connection string 'bitApiContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
